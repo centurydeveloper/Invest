e77d1a2c509041c01820d4ed5b31fa444ca2cb52
Akshay <alamba093@gmail.com>
2023-04-09 17:24:00 +0400

    app check removed for debugging

--- src/middleware/appCheckValidator.js
+++ src/middleware/appCheckValidator.js
@@ -2,30 +2,30 @@ const admin = require("firebase-admin");
 
   function verifyToken(req, res, next) {
     
-    
-    const appCheckToken = req.header("X-AppCheck-token");
+    next();
+  //   const appCheckToken = req.header("X-AppCheck-token");
     
 
-    if(!appCheckToken) {
-      res.status(401).send({message:'UnAuthorized',status:401});
-    }
+  //   if(!appCheckToken) {
+  //     res.status(401).send({message:'UnAuthorized',status:401});
+  //   }
 
 
-   admin.appCheck().verifyToken(appCheckToken)
-   .then(function(result) {
+  //  admin.appCheck().verifyToken(appCheckToken)
+  //  .then(function(result) {
     
-      //result will be `true` if token is valid, non-expired, and has matching claims
-      if(result){
+  //     //result will be `true` if token is valid, non-expired, and has matching claims
+  //     if(result){
         
-        next();
-      }   
-      else{
-        res.status(401).send({message:'UnAuthorized',status:401});
-      }
-      //result will be `false` if token is invalid, expired or fails the claims check
-    }).catch(function(error) {
-      res.status(401).send({message:'UnAuthorized',status:401});
-    })
+  //       next();
+  //     }   
+  //     else{
+  //       res.status(401).send({message:'UnAuthorized',status:401});
+  //     }
+  //     //result will be `false` if token is invalid, expired or fails the claims check
+  //   }).catch(function(error) {
+  //     res.status(401).send({message:'UnAuthorized',status:401});
+  //   })
    
   }
   
